e491baa4b82a94e67b02cce56d95b37506c63556
Alban Cavalli <alban.cavalli7@etu.univ-lorraine.fr>
2023-03-13 23:07:00 +0100

    nextAuthUrl

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,14 +4,14 @@ import GoogleProvider from "next-auth/providers/google";
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
-    // GoogleProvider({
-    //     clientId: process.env.GOOGLE_CLIENT_ID,
-    //     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    //   }),
-      GithubProvider({
-        clientId: process.env.GITHUB_ID,
-        clientSecret: process.env.GITHUB_SECRET
+    GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       }),
+      // GithubProvider({
+      //   clientId: process.env.GITHUB_ID,
+      //   clientSecret: process.env.GITHUB_SECRET
+      // }),
     // ...add more providers here
   ],
   secret: process.env.JWT_SECRET
